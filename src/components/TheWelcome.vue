2b18586669dfb3708e455f515a3d5661e99db7f8
abdulaziz-damlahi <abood_d@live.com>
2023-06-13 23:06:58 +0300

    characters adding api

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,24 +1,37 @@
 <script>
 import {private_key, public_key} from './marvel.js';
 import axios from "axios";
-var url =`https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${public_key}&hash=f854b684a06440517ab7e53d3bb7cb6c`;
-console.log(url)
-export default {
 
+var url = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${public_key}&hash=f854b684a06440517ab7e53d3bb7cb6c`;
+
+export default {
   name: 'TheWelcome',
-  mounted() {
-    this.getCharacters()
-  },
-  methods:{
+  data(){
+  return {
+    char: [],
+  }
+},
+mounted()
+{
+  this.getCharacters()
+}
+,
+methods:{
   getCharacters: function () {
-    axios.get(url)
+    const response = axios.get(url)
         .then((result) => {
-          console.log(result)
+          result.data.data.results.forEach((item)=>{
+            this.char.push(item)
+          })
+          const jsonObj = JSON.stringify(result.data.data.results);
+          console.log(jsonObj)
         }).catch((error) =>
-        console.log(error)
-    )}
+            console.log(error)
+        )
+  }
 }
 }
+
 </script>
 
 <template>
@@ -34,7 +47,7 @@ export default {
       <img src="https://i.ibb.co/h8rkB6h/cityhunter-poster.png">
       <img src="https://i.ibb.co/WVjqddg/cornerworld-poster.png">
     </div>
-    <div><br>
+    <div>
       <h1 class="slidertitle">most popular</h1></div>
     <div class="poster-slide2">
       <img
@@ -50,21 +63,12 @@ export default {
       <img
           src="https://i.ibb.co/WVjqddg/cornerworld-poster.png">
     </div>
-    <div><br>
+    <div>
       <h1 class="slidertitle">characters</h1></div>
     <div class="poster-slide2">
-      <img
-          src="https://i.ibb.co/BrQ9tdN/fireworks-poster.png">
-      <img
-          src="https://i.ibb.co/tbNVqxz/free-poster.png">
-      <img
-          src="https://i.ibb.co/XZP6RCQ/yourname-poster.png">
-      <img
-          src="https://i.ibb.co/61ftHNR/asilentvoice-poster.png">
-      <img
-          src="https://i.ibb.co/h8rkB6h/cityhunter-poster.png">
-      <img
-          src="https://i.ibb.co/WVjqddg/cornerworld-poster.png">
+
+        <img v-for="block in char" v-bind:src="block.thumbnail.path + '.jpg'">
+      {{block.thumbnail.path }}
     </div>
   </div>
 
