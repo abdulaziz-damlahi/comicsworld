bdd6ad8a70c256674e7a6c489659f4161fb6a359
abdulaziz-damlahi <abood_d@live.com>
2023-06-14 01:41:56 +0300

    all api added

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,73 +3,67 @@ import {private_key, public_key} from './marvel.js';
 import axios from "axios";
 
 var url = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${public_key}&hash=f854b684a06440517ab7e53d3bb7cb6c`;
+var url2 = `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${public_key}&hash=f854b684a06440517ab7e53d3bb7cb6c`;
+var url3 = `https://gateway.marvel.com/v1/public/events?ts=1&apikey=${public_key}&hash=f854b684a06440517ab7e53d3bb7cb6c`;
 
 export default {
   name: 'TheWelcome',
-  data(){
-  return {
-    char: [],
+  data() {
+    return {
+      char: [],
+      mag: [],
+      event: [],
+    }
+  },
+  mounted() {
+    this.getCharacters()
+  },
+  methods: {
+    getCharacters: function () {
+      //const response = axios.get(url)
+      axios.all([
+        axios.get(url, {
+          myVar: 'myValue'
+        }), axios.get(url2, {
+          myVar: 'myValue'
+        }),
+        axios.get(url3, {
+          myVar: 'myValue'
+        })
+      ]).then(axios.spread((data1, data2, data3) => {
+        data1.data.data.results.forEach((item1) => {
+          this.char.push(item1)
+        })
+        data2.data.data.results.forEach((item2) => {
+          this.mag.push(item2)
+        })
+        data3.data.data.results.forEach((item3) => {
+          this.event.push(item3)
+        })
+      }))
+    }
   }
-},
-mounted()
-{
-  this.getCharacters()
-}
-,
-methods:{
-  getCharacters: function () {
-    const response = axios.get(url)
-        .then((result) => {
-          result.data.data.results.forEach((item)=>{
-            this.char.push(item)
-          })
-          const jsonObj = JSON.stringify(result.data.data.results);
-          console.log(jsonObj)
-        }).catch((error) =>
-            console.log(error)
-        )
-  }
-}
 }
 
 </script>
 
 <template>
   <div class="outer">
-
     <div><br>
-      <h1 class="slidertitle">latest</h1></div>
-    <div class="poster-slide">
-      <img src="https://i.ibb.co/BrQ9tdN/fireworks-poster.png">
-      <img src="https://i.ibb.co/tbNVqxz/free-poster.png">
-      <img src="https://i.ibb.co/XZP6RCQ/yourname-poster.png">
-      <img src="https://i.ibb.co/61ftHNR/asilentvoice-poster.png">
-      <img src="https://i.ibb.co/h8rkB6h/cityhunter-poster.png">
-      <img src="https://i.ibb.co/WVjqddg/cornerworld-poster.png">
+      <h1 class="slidertitle">events</h1></div>
+    <div class="poster-slide2 scrollmenu">
+      <img v-for="block in event" v-bind:src="block.thumbnail.path + '.jpg'">
     </div>
-    <div>
-      <h1 class="slidertitle">most popular</h1></div>
-    <div class="poster-slide2">
-      <img
-          src="https://i.ibb.co/BrQ9tdN/fireworks-poster.png">
-      <img
-          src="https://i.ibb.co/tbNVqxz/free-poster.png">
-      <img
-          src="https://i.ibb.co/XZP6RCQ/yourname-poster.png">
-      <img
-          src="https://i.ibb.co/61ftHNR/asilentvoice-poster.png">
-      <img
-          src="https://i.ibb.co/h8rkB6h/cityhunter-poster.png">
-      <img
-          src="https://i.ibb.co/WVjqddg/cornerworld-poster.png">
+    <div><br>
+      <h1 class="slidertitle ">magazines</h1></div>
+    <div class="poster-slide scrollmenu">
+      <img v-for="block in mag" v-bind:src="block.thumbnail.path + '.jpg'">
+      <div class=""></div>
     </div>
-    <div>
+    <div><br>
       <h1 class="slidertitle">characters</h1></div>
-    <div class="poster-slide2">
-
-        <img v-for="block in char" v-bind:src="block.thumbnail.path + '.jpg'">
-      {{block.thumbnail.path }}
+    <div class="poster-slide2 scrollmenu">
+      <img v-for="block in char" v-bind:src="block.thumbnail.path + '.jpg'">
     </div>
   </div>
-
 </template>
